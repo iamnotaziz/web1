3c30e28d7ce74a232b2e043d4f0ff3f7cea0f6b5
iamnotaziz <azizammar.contact@gmail.com>
2024-02-19 11:56:57 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Function to change background color
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightblue";
+  }
+  // Event listener for button click
+  document
+    .getElementById("changeColorBtn")
+    .addEventListener("click", changeBackgroundColor);
+  // Form validation
+  document
+    .getElementById("submitBtn")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent form submission
+      var nameInput = document.getElementById("name");
+      var emailInput = document.getElementById("email");
+      if (nameInput.value.trim() === "" || emailInput.value.trim() === "") {
+        //trim
+        alert("Please fill in all required fields.");
+      } else {
+        alert("Form submitted successfully!");
+      }
+    });
+  // Image hover effect
+  var image = document.getElementById("image");
+  image.addEventListener("mouseover", function () {
+    image.src = "new_image.jpg";
+  });
+  image.addEventListener("mouseout", function () {
+    image.src = "image.jpg";
+  });
